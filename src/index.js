c2df324bf5df5342e035a5c0e64c7074bb81e135
Dzmitry Bryt <bdv212@yandex.ru>
2019-10-10 16:14:36 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function multiply(first, second) {
   // your solution
+  // BigInt type is a numeric primitive. 
+  // BigInts, you can safely store and operate on large integers even beyond the safe integer limit for Numbers.
+  //  A BigInt is created by appending n to the end of an integer or by calling the constructor.
+  
+  let mult = (BigInt(first) * BigInt(second)).toString();
+
+  return mult;
 }
